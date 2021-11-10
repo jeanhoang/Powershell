f5f79d6b1552a68b4700332417046fa159697687
jeanhoang <jeantknhoang@gmail.com>
2021-11-10 11:57:59 -0500

    Adding comments

--- usefulCmdlets.ps.ps1
+++ usefulCmdlets.ps.ps1
@@ -1,4 +1,9 @@
+#Cause I keep hearing people saying they couldn't find a person on AD
+#Usually finding by employeeID is easier
+
+
 #Find user based on employeeID
+#Play with it like how you play with CRM ID look up
 Get-AdUser -filter { employeeid -eq <#employeeID#> } |  Select-Object SamAccountName
 
 #Find which OU a user is located
